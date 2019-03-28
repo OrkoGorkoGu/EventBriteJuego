733495ea091e371cce26b1119fe25e24dbbbbc95
Adam Lathan <lathan.adam@gmail.com>
2019-03-28 09:57:11 -0300

    Cambié los adivinanzas ser puramente aleatorio

--- Juego.py
+++ Juego.py
@@ -30,33 +30,35 @@ def main():
         print("¡Felicidades! Has adivinado mi número.")
 
     else:
+        # Iniciar el alcance para adivinar
         min = 1
         max = 100
-        # Adivinar el número del usario
-        compGuess = random.randint(1,100)
 
-        print("\nMi adivinanza es %s" %compGuess)
-        print("¿Tengo razón?")
-        print("\ts\tSí, adiviné el número")
-        print("\tg\tNo, mi adivinanza fue demasiado grande.")
-        print("\tc\tNo, mi adivinanza fue demasiado chico.")
-        res = input()
-        
-        while not (res.lower() == "s"):
-            
-            if res.lower() == "g":
-                max = compGuess
-                compGuess -= int((compGuess - min)/2)
-            elif res.lower() == "c":
-                min = compGuess
-                compGuess += int((max - compGuess)/2)
-            
+        while True:  
+            # Adivinar el número del usario
+            compGuess = random.randint(min,max)
+
             print("\nMi adivinanza es %s" %compGuess)
             print("¿Tengo razón?")
             print("\ts\tSí, adiviné el número")
             print("\tg\tNo, mi adivinanza fue demasiado grande.")
             print("\tc\tNo, mi adivinanza fue demasiado chico.")
             res = input()  
+
+            # PC ha adivinado el número
+            if res == "s":
+                # Juego se acabó
+                break
+            
+            # Adivinanza de la PC es demasiado grande
+            elif res.lower() == "g":
+                # Cambiar el máximo
+                max = compGuess - 1
+            
+            # Adivinanza de la PC es demasiado chico
+            elif res.lower() == "c":
+                # Cambiar el mínimo
+                min = compGuess + 1
 
     print("¡Gracias por jugar conmigo! Chau.")
     return
